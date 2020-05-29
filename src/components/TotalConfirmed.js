4ee384b1e96a7b63d9d5475b168db2bb39407042
noah-erasmus <noah.erasmus@gmail.com>
2020-05-29 18:08:14 +0200

    Added percentage change for Total Confirmed

--- src/components/TotalConfirmed.js
+++ src/components/TotalConfirmed.js
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -17,6 +18,8 @@ export const TotalConfirmed = () => {
   const bull = <span className={classes.bullet}>•</span>;
   const [api, setApi] = useState({})
   const [totalConfirmed, setTotalConfirmed] = useState(0)
+  const [percentageIncrease, setPercentageIncrease] = useState(0)
+  const [percentageOperator, setPercentageOperator] = useState('+')
 
   useEffect(() => {
       console.log("This component rendered.")
@@ -27,6 +30,15 @@ export const TotalConfirmed = () => {
       .then(data => {
           setApi(data)
           setTotalConfirmed(data.Global.TotalConfirmed)
+          const percentage = (data.Global.NewConfirmed/data.Global.TotalConfirmed*100).toFixed(2)
+          
+          setPercentageIncrease(percentage)
+
+          if(percentage > 0){
+            percentageOperator = '+'
+          }else{
+            percentageOperator = '-'
+          }
           
 
       })
@@ -39,12 +51,24 @@ export const TotalConfirmed = () => {
   return(
     <Card className={classes.root} variant="outlined">
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Worldwide Confirmed Cases
-        </Typography>
-        <Typography variant="h5" component="h2">
-          {totalConfirmed}
-        </Typography>
+        <Grid container spacing={2}> 
+          <Grid item>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Worldwide Confirmed Cases
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {totalConfirmed}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography>
+              {percentageOperator}{percentageIncrease}%
+            </Typography>
+            <Typography>
+              from yesterday
+            </Typography>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   )
